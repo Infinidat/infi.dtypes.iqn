7fe3097fbbd936f3d90af4a763d8505eecfdb2ec
Ayala Shachar <shachar.ayala@gmail.com>
2016-12-28 13:23:05 +0200

    Support iSCSIName

--- infi/dtypes/iqn/__init__.py
+++ infi/dtypes/iqn/__init__.py
@@ -3,13 +3,35 @@ class InvalidIQN(ValueError):
         super(InvalidIQN, self).__init__('Invalid IQN {}'.format(name))
 
 
-class IQN(object):
+class iSCSIName(object):
     def __init__(self, name):
-        super(IQN, self).__init__()
-        if isinstance(name, IQN):
-            self._name = name._name
+        super(iSCSIName, self).__init__()
+        if isinstance(name, iSCSIName):
+            self._name = name._name  # pylint: disable=protected-access
         else:
             self._name = name
+
+    def __repr__(self):
+        return self._name
+
+    def __hash__(self):
+        return hash(self._name)
+
+    def __eq__(self, other):
+        if not isinstance(other, iSCSIName):
+            try:
+                other = iSCSIName(other)
+            except Exception:  # pylint: disable=broad-except
+                return False
+        return self._name == other._name  # pylint: disable=protected-access
+
+    def __ne__(self, other):
+        return not (self == other)  # pylint: disable=superfluous-parens
+
+
+class IQN(iSCSIName):
+    def __init__(self, name):
+        super(IQN, self).__init__(name)
         fields = self._name.split(':')
         base, self._extra = fields[0], tuple(fields[1:])
         base_fields = base.split('.')
@@ -20,23 +42,6 @@ class IQN(object):
         self._naming_authority = '.'.join(base_fields[2:])
         if self._type != 'iqn':
             raise InvalidIQN(name)
-
-    def __repr__(self):
-        return self._name
-
-    def __hash__(self):
-        return hash(self._name)
-
-    def __eq__(self, other):
-        if not isinstance(other, IQN):
-            try:
-                other = IQN(other)
-            except:
-                return False
-        return self._name == other._name
-
-    def __ne__(self, other):
-        return not (self == other)
 
     def get_date(self):
         return self._date
@@ -49,3 +54,10 @@ class IQN(object):
 
     def get_extra_fields(self):
         return self._extra
+
+
+def make_iscsi_name(iscsi_name):
+    try:
+        return IQN(iscsi_name)
+    except InvalidIQN:
+        return iSCSIName(iscsi_name)
--- tests/test_iqn.py
+++ tests/test_iqn.py
@@ -1,4 +1,7 @@
-from infi.dtypes.iqn import IQN
+import pytest
+import itertools
+from infi.dtypes.iqn import IQN, iSCSIName, InvalidIQN, make_iscsi_name  # pylint: disable=no-name-in-module
+
 
 def test_basic():
     a = IQN('iqn.2001-04.com.example:storage:diskarrays-sn-a8675309')
@@ -11,6 +14,7 @@ def test_basic():
     assert a.get_extra() == 'storage:diskarrays-sn-a8675309'
     assert a.get_extra_fields() == ('storage', 'diskarrays-sn-a8675309')
 
+
 def test_rfc_examples():
     a = IQN('iqn.2001-04.com.example')
     assert a.get_date() == '2001-04'
@@ -18,6 +22,27 @@ def test_rfc_examples():
     assert a.get_extra() == ''
     assert a.get_extra_fields() == ()
 
-def test_copy_ctor():
-    a = IQN('iqn.2001-04.com.example')
-    assert IQN(a) == a
+
+@pytest.mark.parametrize('copy_types', itertools.product([IQN, iSCSIName], repeat=2))
+def test_copy_ctor(copy_types):
+    source_type, target_type = copy_types
+    obj = source_type('iqn.2001-04.com.example')
+    assert target_type(obj) == obj
+
+
+def test_invalid_iqn():
+    iscsi_identifier = '1'
+    with pytest.raises(InvalidIQN):
+        IQN(iscsi_identifier)
+    iSCSIName(iscsi_identifier)
+    assert isinstance(make_iscsi_name(iscsi_identifier), iSCSIName)
+
+
+def test_equality():
+    iscsi_identifier = 'iqn.2001-04.com.example'
+    iqn = IQN(iscsi_identifier)
+    iscsi_name = iSCSIName(iscsi_identifier)
+    assert iqn == iscsi_name
+    assert iqn == iscsi_identifier
+    assert iscsi_name == iqn
+    assert iscsi_name == iscsi_identifier
